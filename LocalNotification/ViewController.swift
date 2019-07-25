a5b6dd2190b701501696c8a23bcc3f9f142eaee4
辻真緒 <>
2019-07-25 11:52:10 +0800

    AppDelegate.swiftに、通知許可プログラム追加

--- LocalNotification/ViewController.swift
+++ LocalNotification/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldForTitle: UITextField!
+    @IBOutlet weak var textFieldForContent: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func didClickButton(_ sender: UIButton) {
+        
+        
+    }
+    
 }
 
